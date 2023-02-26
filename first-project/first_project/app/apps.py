d9253b4673c0df3d2974f055690779a40966f684
Marat Muratov <muramr@mail.ru>
2023-02-26 21:02:31 +0500

    first commit

---
+++ first-project/first_project/app/apps.py
@@ -0,0 +1,14 @@
+from django.apps import AppConfig
+
+
+class AppConfig(AppConfig):
+    name = 'app'
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'newapp',  # new app
+]
